6a16b25a609edb91c9015a66d5426bc576cafb3e
Adam Smith <asmith6@luc.edu>
2021-02-22 01:10:41 -0600

    Adding in class skeletons

---
+++ src/main/java/adamSmithComp373Proj1/Domain/FacilityMaintainence.java
@@ -0,0 +1,45 @@
+package Domain;
+
+public class FacilityMaintainence {
+ 
+    
+
+
+
+
+    public Object makeFacilityMaintRequest(){
+
+    }
+
+    public Object scheduleMaintenance(){
+
+    }
+
+    public Object calcMaintenanceCostForFacility(){
+
+    }
+
+    public Object calcProblemRateForFacility(){
+
+    }
+
+    public Object calcDownTimeForFacility(){
+
+    }
+
+    public Object listMaintRequests(){
+
+    }
+
+    public Object listMaintenance(){
+
+    }
+
+    public Object listFacilityProblems(){
+        
+    }
+
+
+
+
+}
---
+++ src/main/java/adamSmithComp373Proj1/Domain/FacilityUse.java
@@ -0,0 +1,31 @@
+package Domain;
+
+public class FacilityUse {
+ 
+    
+
+    public Object isInUseDuringInterval(){
+
+    }
+    
+    public Object assignFacilityToUse(){
+
+    }
+    
+    public Object vacateFacility(){
+
+    }
+    
+    public Object listInspections(){
+
+    }
+    
+    public Object listActualUsage(){
+
+    }
+    
+    public Object calcUsageRate(){
+        
+    }
+
+}
