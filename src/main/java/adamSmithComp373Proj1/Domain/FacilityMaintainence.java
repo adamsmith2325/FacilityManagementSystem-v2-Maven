b20f45c39084f7efb923b484a6cf033a12f9426b
Adam Smith <asmith6@luc.edu>
2021-02-27 18:14:20 -0600

    Interface skeleton, updated UML

--- src/main/java/adamSmithComp373Proj1/Domain/FacilityMaintainence.java
+++ src/main/java/adamSmithComp373Proj1/Domain/FacilityMaintainence.java
@@ -1,8 +1,9 @@
 package adamSmithComp373Proj1.Domain;
+import adamSmithComp373Proj1.Domain.*;
 
 enum RequestType {Repair, Cleaning, BrokenItem}
 
-public class FacilityMaintainence {
+public class FacilityMaintainence extends Facility {
  
     private Integer RequestID;
     private Facility facility;
