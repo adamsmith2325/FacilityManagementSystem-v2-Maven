0ef925478dcd1ad6707592c5bc33270d2a7d5e41
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-02-28 20:52:20 -0600

    just a few changes
    nothing too major

--- src/main/java/adamSmithComp373Proj1/Domain/FacilityUse.java
+++ src/main/java/adamSmithComp373Proj1/Domain/FacilityUse.java
@@ -1,9 +1,16 @@
 package adamSmithComp373Proj1.Domain;
 
 public class FacilityUse {
- 
-    
 
+    public Facility getFacility();
+    public int getUseID();
+    public int getFacilityID();
+    public int getRoomNumber();
+    
+    
+    public void setFacility(Facility facility){
+        
+    };
     public Object isInUseDuringInterval(){
 
     }
@@ -27,5 +34,6 @@ public class FacilityUse {
     public Object calcUsageRate(){
         
     }
+
 
 }
