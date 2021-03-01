ef43d625290b081f3b05cc5199fbd9efeab4a6aa
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-03-01 15:09:17 -0600

    FacilityUse updated

--- src/main/java/adamSmithComp373Proj1/Domain/FacilityUse.java
+++ src/main/java/adamSmithComp373Proj1/Domain/FacilityUse.java
@@ -14,10 +14,10 @@ public class FacilityUse {
 
     private int useID;
     private static FacilityUseDAO Connection = new FacilityUseDAO();
-    private Facility facility;
+    private Facility facility  = Connection.getFacility(this.useID);
     private Date dateStarted = Connection.getDateStarted(this.useID);
-    private Date dateEnded;
-    private int roomID;
+    private Date dateEnded  = Connection.getDateEnded(this.useID);
+    private int roomID  = Connection.getRoomID(this.useID);
     
 
     public FacilityUse(Integer ID){
@@ -36,12 +36,11 @@ public class FacilityUse {
 
     public Date getDateEnded() {
 
-        return dateEnded;
+        return Connection.getDateEnded(this.useID);
     }
 
     public void setDateEnded(Date dateEnded) {
-
-        this.dateEnded = dateEnded;
+        Connection.setDateEnded(this.dateEnded);
     }
 
     public int getUseID() {
@@ -50,7 +49,7 @@ public class FacilityUse {
     }
     public void setUseID(int useID) {
         
-        this.useID = useID;
+        Connection.setUseID(this.useID);
     }
 
     public Facility getFacility() {
@@ -69,14 +68,13 @@ public class FacilityUse {
         
     }
 
-    public int getRoomID() {
-
+    public int getRoomID(Integer roomID) {
+        Connection.getRoomID(this.roomID);
         return roomID;
     }
 
     public void setRoomID(int facID) {
-
-        this.roomID = facID;
+        Connection.setRoomID(this.useID);
     }
 
 }
