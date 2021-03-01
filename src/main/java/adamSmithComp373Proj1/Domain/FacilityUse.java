8108b3055320951a3609279172b926a1a562df9e
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-02-28 21:13:01 -0600

    updates

--- src/main/java/adamSmithComp373Proj1/Domain/FacilityUse.java
+++ src/main/java/adamSmithComp373Proj1/Domain/FacilityUse.java
@@ -4,24 +4,45 @@ import java.time.LocalDate;
 
 public class FacilityUse {
 
-
+    private Facility facility;
     private LocalDate dateStarted;
     private LocalDate dateEnded;
+    private int facID;
+    private int locationID;
 
     public LocalDate getDateStarted() {
+
         return dateStarted;
     }
 
     public void setDateStarted(LocalDate dateStarted) {
+
         this.dateStarted = dateStarted;
     }
 
     public LocalDate getDateEnded() {
+
         return dateEnded;
     }
 
     public void setDateEnded(LocalDate dateEnded) {
+
         this.dateEnded = dateEnded;
     }
 
-}  
+    public Facility getFacility() {
+
+        return facility;
+    }
+
+    public int getLocationID() {
+
+        return locationID;
+    }
+
+    public void setLocationID(int locationID) {
+
+        this.locationID = locationID;
+    }
+
+}
