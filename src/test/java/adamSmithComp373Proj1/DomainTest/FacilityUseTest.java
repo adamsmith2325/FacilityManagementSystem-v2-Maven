a054508a831f6d741ab8e7575f1a1405f851d887
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-03-01 23:01:06 -0600

    updated FacilityUseTest

--- src/test/java/adamSmithComp373Proj1/DomainTest/FacilityUseTest.java
+++ src/test/java/adamSmithComp373Proj1/DomainTest/FacilityUseTest.java
@@ -1,5 +1,21 @@
 package adamSmithComp373Proj1.DomainTest;
 
-public class FacilityUseTest {
-    
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class FacilityUseTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+        assertEquals(2, 2);
+        assertEquals(3, 3);
+        assertEquals(4, 4);
+    }
 }
