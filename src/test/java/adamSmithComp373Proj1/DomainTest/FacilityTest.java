cf3df7e00e9fe45e51014bf478db91777e2e60c8
Adam Smith <asmith6@luc.edu>
2021-03-01 18:55:57 -0600

    Facility Unit Testing Done

--- src/test/java/adamSmithComp373Proj1/DomainTest/FacilityTest.java
+++ src/test/java/adamSmithComp373Proj1/DomainTest/FacilityTest.java
@@ -16,16 +16,16 @@ class FacilityTest {
     
     Facility testFacility = new Facility(2);
 
-
+    //Main Functionality Tests 2
     @Test
     void mainFunctionalities(){
-        assertEquals("[Adam Test 1 - id: 1, Adam Set Test 1 - id: 2]", testFacility.listFacilities().toString());
+        assertEquals("[Adam Test 1 - id: 1, Adam Set Test 1 - id: 2, Adam Unit Test 2 - id: 4]", testFacility.listFacilities().toString());
         testFacility.addNewFacility("Adam Test 3", "112 9th Avenue, New York, NY 10011", "8473901289", 12);
-        assertEquals("[Adam Test 1 - id: 1, Adam Set Test 1 - id: 2, Adam Test 3 - id: 3]", testFacility.listFacilities().toString());   
-        Facility testFacility2  = new Facility(3);
+        assertEquals("[Adam Test 1 - id: 1, Adam Set Test 1 - id: 2, Adam Unit Test 2 - id: 4, Adam Test 3 - id: 14]", testFacility.listFacilities().toString());   
+        Facility testFacility2  = new Facility(14);
         assertEquals(12, testFacility2.requestAvailableCapacity());
         testFacility2.removeFacility();
-        assertEquals("[Adam Test 1 - id: 1, Adam Set Test 1 - id: 2]", testFacility.listFacilities().toString());   
+        assertEquals("[Adam Test 1 - id: 1, Adam Set Test 1 - id: 2, Adam Unit Test 2 - id: 4]", testFacility.listFacilities().toString());   
     }
 
 
@@ -64,6 +64,70 @@ class FacilityTest {
             assertEquals(3, testFacility.getNumberOfRooms());
         }
 
+        
+
+
+
+        //Second Round of Testing
+
+
+
+
+        Facility UnitTestFac = new Facility(4);
+
+
+
+
+
+        //Main Functionality Tests 2
+        @Test
+        void mainFunctionalities2(){
+            assertEquals("[Adam Test 1 - id: 1, Adam Set Test 1 - id: 2, Adam Unit Test 2 - id: 4]", UnitTestFac.listFacilities().toString());
+            UnitTestFac.addNewFacility("Adam Test 4", "112 9th Avenue, New York, NY 10011", "8473901289", 12);
+            assertEquals("[Adam Test 1 - id: 1, Adam Set Test 1 - id: 2, Adam Unit Test 2 - id: 4, Adam Test 4 - id: 15]", UnitTestFac.listFacilities().toString());   
+            Facility UnitTestFac2  = new Facility(15);
+            assertEquals(12, UnitTestFac2.requestAvailableCapacity());
+            UnitTestFac2.removeFacility();
+            assertEquals("[Adam Test 1 - id: 1, Adam Set Test 1 - id: 2, Adam Unit Test 2 - id: 4]", testFacility.listFacilities().toString());   
+        }
+    
+    
+
+
+            //Occupied Room Tests 2
+            @Test
+            void OccupiedRoomTests2() {
+                UnitTestFac.addOccupiedRoom();
+                assertEquals(1, UnitTestFac.getOccupiedRooms());
+                UnitTestFac.addOccupiedRoom();
+                assertEquals(2, UnitTestFac.getOccupiedRooms());
+                UnitTestFac.addOccupiedRoom();
+                assertEquals(3, UnitTestFac.getOccupiedRooms());
+                UnitTestFac.addOccupiedRoom();
+                assertEquals(3, UnitTestFac.getOccupiedRooms());
+                UnitTestFac.voidOccupiedRoom();
+                assertEquals(2, UnitTestFac.getOccupiedRooms());
+                UnitTestFac.voidOccupiedRoom();
+                assertEquals(1, UnitTestFac.getOccupiedRooms());
+                UnitTestFac.voidOccupiedRoom();
+                assertEquals(0, UnitTestFac.getOccupiedRooms());
+                UnitTestFac.voidOccupiedRoom();
+                assertEquals(0, UnitTestFac.getOccupiedRooms());
+            }
+    
+            //Get Set Tests 2
+            @Test
+            void getSetTests2() {
+                UnitTestFac.setName("Adam Unit Test 2");
+                assertEquals("Adam Unit Test 2", UnitTestFac.getName());
+                UnitTestFac.setLocation("2222 W Sheridan Ave, Chicago, IL 60626");
+                assertEquals("2222 W Sheridan Ave, Chicago, IL 60626", UnitTestFac.getLocation());
+                UnitTestFac.setPhone("1111211111");
+                assertEquals("1111211111", UnitTestFac.getPhone());
+                UnitTestFac.setNumberOfRooms(3);
+                assertEquals(3, UnitTestFac.getNumberOfRooms());
+            }
+    
 
 
     }
