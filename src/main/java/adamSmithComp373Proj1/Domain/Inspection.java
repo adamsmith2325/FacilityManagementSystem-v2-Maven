63f503e8680ebf58e4e84073757e65124780267c
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-03-01 21:46:48 -0600

    another update to inspection

--- src/main/java/adamSmithComp373Proj1/Domain/Inspection.java
+++ src/main/java/adamSmithComp373Proj1/Domain/Inspection.java
@@ -4,45 +4,32 @@ import adamSmithComp373Proj1.Domain.Facility;
 
 public enum Type{TYPE, TYPE2, TYPE3}
 
-public class FacilityInspection{
+public class Inspection {
+    
+    private InspectionDAO Conn = new InspectionDAO();
+    private Integer InspectionID;
+    private String Name = Conn.getName();
+    private Type InsType = Conn.getType();
+    private String Details = Conn.getDetails();
+    private Integer facility = Conn.getFacility();
+    private Integer Room = Conn.getRoom();
+    
+    public void addInspection(){
+        
+    }
+    public String getName() {
+        return this.Name;
+    }
+    public Type getType() {
+        return this.InsType;
+    }
+    public String getDetails() {
+        return this.Details;
+    }
+    public int getFacility() {
+        return this.facility;
+    }
 
-    //attributes needed
-    private int inspectionID;
-    private int facilityID;
-    private String inspection_type;
-    private String inspection_detail;
-    private Facility facility;
-
-
-//Still need to edit
-    public Facility getFacility() {
-        return facility;
+    public Integer getRoom(){
+        return this.Room;
     }
-    public void setFacility(Facility facility) {
-        this.facility = facility;
-    }
-    public int getFacilityID() {
-        return facilityID;
-    }
-    public void setFacilityID(int facilityID) {
-        this.facilityID = facilityID;
-    }
-    public String getInspection_type() {
-        return inspection_type;
-    }
-    public void setInspection_type(String inspection_type) {
-        this.inspection_type = inspection_type;
-    }
-    public String getInspection_detail() {
-        return inspection_detail;
-    }
-    public void setInspection_detail(String inspection_detail) {
-        this.inspection_detail = inspection_detail;
-    }
-    public int getInspectionID() {
-        return inspectionID;
-    }
-    public void setInspectionID(int inspectionID) {
-        this.inspectionID = inspectionID;
-    }
-}
