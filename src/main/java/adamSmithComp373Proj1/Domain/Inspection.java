d200ae5331ef5aa42796dfda8f2d1c73a14e6ce0
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-03-01 21:30:14 -0600

    whoops missed something

--- src/main/java/adamSmithComp373Proj1/Domain/Inspection.java
+++ src/main/java/adamSmithComp373Proj1/Domain/Inspection.java
@@ -1,6 +1,8 @@
-package com.fms.usage;
+package adamSmithComp373Proj1.Domain;
+import adamSmithComp373Proj1.DAL.InspectionDAO;
+import adamSmithComp373Proj1.Domain.Facility;
 
-import com.fms.main.Facility;
+public enum Type{TYPE, TYPE2, TYPE3}
 
 public class FacilityInspection{
 
